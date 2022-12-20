a1596abed56aca8fc64f7601b03e032ccdb6ef02
Gencer KARAKAYA <gencerkrky@gmail.com>
2022-12-20 20:00:32 +0300

    #17 Feature Statistics

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager:IDestinationService
+    {
+        IDestintionDal _destintionDal;
+
+        public DestinationManager(IDestintionDal destintionDal)
+        {
+            _destintionDal= destintionDal;
+        }
+
+        public void TAdd(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Destination TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> TGetList()
+        {
+           return _destintionDal.GetList();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
