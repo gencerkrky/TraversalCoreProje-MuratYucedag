a8dc1089ed67ec738051a4420af91aa35475145e
Gencer KARAKAYA <gencerkrky@gmail.com>
2022-12-23 19:46:09 +0300

    #32 add-userLayout

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length) => new IdentityError 
+        {
+            Code = "PasswordTooShort", 
+            Description = $"Parola en az {length} karakter olmalıdır" 
+        };
+        public override IdentityError PasswordRequiresUpper () => new IdentityError
+        {
+            Code = "PasswordRequiresUpper",
+            Description = $"Parola en az 1 büyük karakter olmalıdır"
+        };
+        public override IdentityError PasswordRequiresLower() => new IdentityError
+        {
+            Code = "PasswordRequiresUpper",
+            Description = $"Parola en az 1 küçük karakter olmalıdır"
+        };
+        public override IdentityError PasswordRequiresNonAlphanumeric() => new IdentityError
+        {
+            Code = "PasswordRequiresUpper",
+            Description = $"Parola en az 1 sembol olmalıdır"
+        };
+    }
+}
