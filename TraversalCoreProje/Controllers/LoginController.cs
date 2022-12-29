14f3fe938ab43042db2e0612ad1359fb8d9a4844
Gencer KARAKAYA <gencerkrky@gmail.com>
2022-12-29 13:25:03 +0300

    #34 Identity ile Login İşlemi

--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -11,13 +11,15 @@ namespace TraversalCoreProje.Controllers
 	public class LoginController : Controller
 	{
 		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
 
-		public LoginController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-		[HttpGet]
+        [HttpGet]
 		public IActionResult SignUp()
 		{
 			return View();
@@ -53,6 +55,23 @@ namespace TraversalCoreProje.Controllers
 		[HttpGet]
 		public IActionResult SignIn()
 		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> SignIn(UserSingInViewModel p)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index","Destination");
+				}
+				else
+				{
+					return RedirectToAction("SingIn", "Login");
+				}
+			}
 			return View();
 		}
 	}
---
+++ TraversalCoreProje/Models/UserSingInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSingInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullnıcı adını giriniz")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string password { get; set; }
+    }
+}
