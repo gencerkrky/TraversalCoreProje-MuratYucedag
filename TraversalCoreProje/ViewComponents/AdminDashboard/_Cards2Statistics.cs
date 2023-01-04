7130eac908feb0ccb18015b4d9a1a5dcc68ee6ac
Gencer KARAKAYA <gencerkrky@gmail.com>
2023-01-05 00:21:43 +0300

    #53 Admin Dashboard

---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_Cards2Statistics.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _Cards2Statistics : ViewComponent
+    {
+       
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_TotalRevenue.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _TotalRevenue : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
